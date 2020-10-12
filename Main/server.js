14558db0953fd48e9f920f972b25a2a5d79c3133
Marylee Pabst <marylee@uconn.edu>
2020-10-11 19:45:22 -0700

    updated server info

--- Main/server.js
+++ Main/server.js
@@ -23,9 +23,69 @@ app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
 
 //routes 
+//render script
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+//display script
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        return res.json(JSON.parse(data))
+    });
+})
+
+//post new
+app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        let objectList = JSON.parse(data);
+        let newNote = req.body
+
+        newNote.id = (formatId + amountOfNotes);
+        amountOfNotes += 1;
+
+        objectList.push(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(arrayOfObjects), "utf8", (err) => {
+            if (err) throw err;
+            console.log('New db file')
+            return res.json(objectList)
+        })
+    });
+});
+
+//delete selected
+app.delete("/api/notes/:id", (req, res) => {
+    const chosen = req.params.id;
+    console.log(chosen);
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        let objectList = JSON.parse(data);
+        let spliceLocation;
+
+
+        for (let i = 0; i < objectList.length; i++) {
+            if (chosen === objectList[i].id) {
+                deleteIt = i;
+                console.log(deleteIt)
+            }
+        };
+
+        objectList.splice(deleteIt, 1);
+
+        fs.writeFile("./db/db.json", JSON.stringify(arrayOfObjects), "utf8", (err) => {
+            if (err) throw err;
+            return res.json(objectList)
+        })
+    });
+});
+
+
